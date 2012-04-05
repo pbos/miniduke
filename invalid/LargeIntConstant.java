46086a66f62fab8f428b5d7f069d7b77fff013eb
Peter Boström <pbos@kth.se>
2012-04-05 11:32:01 +0200

    error test cases

---
+++ invalid/LargeIntConstant.java
@@ -0,0 +1,5 @@
+class LargeIntConstant {
+    public static void main(String[] a){
+	System.out.println(2147483648); // 2**31, integer too large
+    }
+}
---
+++ invalid/NegativeIntConstant.java
@@ -0,0 +1,5 @@
+class NegativeIntConstant {
+    public static void main(String[] a){
+		System.out.println(-1); // Negative int constant, invalid in MiniJava
+    }
+}
